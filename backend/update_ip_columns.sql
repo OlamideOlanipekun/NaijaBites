0920395510a92b7ebef588255a046b9a58498462
Olamide Olanipekun <bondpierce0@gmail.com>
2026-02-04 13:46:19 +0100

    Delete backend directory

--- backend/update_ip_columns.sql
+++
@@ -1,3 +0,0 @@
--- Add ip_address to contact_messages and testimonials
-ALTER TABLE contact_messages ADD COLUMN ip_address VARCHAR(45) NULL AFTER message;
-ALTER TABLE testimonials ADD COLUMN ip_address VARCHAR(45) NULL AFTER content;
--- backend/update_subscribers_ip.sql
+++
@@ -1,2 +0,0 @@
--- Add ip_address column to subscribers table
-ALTER TABLE subscribers ADD COLUMN ip_address VARCHAR(45) NULL AFTER email;
